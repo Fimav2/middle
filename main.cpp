d6c0a3920bb8d449dba9708a4ef42a82ea1ae61a
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-11-02 15:01:40 +0300

    middle

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "middle.h"
+using namespace std;
+
+int main()
+{
+cout<<   itc_multi_num(1234);
+
+    return 0;
+
+}
---
+++ middle.h
@@ -0,0 +1,12 @@
+#ifndef MIDDLE_H_INCLUDED
+#define MIDDLE_H_INCLUDED
+
+void itc_num_print(int number);
+
+int itc_len_num(long long number);
+
+int itc_sum_num(long long number);
+
+long long itc_multi_num(long long number);
+
+#endif
