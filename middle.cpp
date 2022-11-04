461df2a1378ada5c8d850c9ae15fc0cc71a4a0a8
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-11-04 17:37:40 +0300

    Update middle.cpp

--- middle.cpp
+++ middle.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include "middle.h"
-using namespace std;
-void itc_num_print(int number){
-cout<< number;
 
+using namespace std;
+
+
+void itc_num_print(int number){
+    cout<< number;
 }
+
+
 int itc_len_num(long long number){
-    int rezult,zifra;
+    int rezult;
     rezult=0;
-while (number>0){
-        zifra = number%10;
+    while (number>0){
         rezult = rezult +1;
         number = number/10;
 }
-return rezult;
-
+    return rezult;
 }
 int itc_sum_num(long long number){
 int symma,zifra;
@@ -37,15 +39,22 @@ while (number>0){
     zifra = number %10;
     proiz = proiz * zifra ;
     number = number /10;
-
-
-
 }
 return proiz;
-
-
-
 }
 
+
+int itc_max_num(long long number){
+    int zifra, great;
+    great = 0;
+    while(number>0){
+        zifra = number % 10;
+        if (zifra > great){
+            great = zifra;
+        }
+        number /=  10;
+    }
+    return great;
+}
 
 
