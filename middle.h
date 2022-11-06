2a2b302374d6b46ace1be31b4600dd39e075c885
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-11-06 20:43:40 +0300

    Update middle.h

--- middle.h
+++ middle.h
@@ -9,4 +9,16 @@ int itc_sum_num(long long number);
 
 long long itc_multi_num(long long number);
 
+int itc_max_num(long long number);
+
+int itc_min_num(long long number);
+
+int itc_rev_num(long long number);
+
+int itc_null_count(long long number);
+
+bool itc_mirror_num(long long number);
+
+
+
 #endif
