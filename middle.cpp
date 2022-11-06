d2c92c4cf8f4199691999086eff3f6598475455d
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-11-06 20:52:37 +0300

    Update middle.cpp

--- middle.cpp
+++ middle.cpp
@@ -3,56 +3,57 @@
 
 using namespace std;
 
+int itc_min_num(long long number){
 
-void itc_num_print(int number){
-    cout<< number;
-}
+    int zifra,minimum;
+    minimum = 9;
 
-
-int itc_len_num(long long number){
-    int rezult;
-    rezult=0;
-    while (number>0){
-        rezult = rezult +1;
-        number = number/10;
-}
-    return rezult;
-}
-int itc_sum_num(long long number){
-int symma,zifra;
-symma = 0 ;
-while (number>0){
-        zifra = number %10;
-        symma = zifra + symma;
-        number = number/10;
-}
-return symma;
-
-
-
-
-}
-long long itc_multi_num(long long number){
-int zifra,proiz;
-    proiz = 1;
-while (number>0){
-    zifra = number %10;
-    proiz = proiz * zifra ;
-    number = number /10;
-}
-return proiz;
-}
-
-
-int itc_max_num(long long number){
-    int zifra, great;
-    great = 0;
-    while(number>0){
+    while (number >0){
         zifra = number % 10;
-        if (zifra > great){
-            great = zifra;
+
+        if(zifra<minimum){
+            minimum = zifra;
         }
-        number /=  10;
+
+        number /= 10;
     }
-    return great;
+
+    return minimum;
+
 }
+
+int itc_rev_num(long long number){
+
+    return -1;
+
+}
+
+int itc_null_count(long long number){
+    int zifra, nullCount;
+    nullCount = 0;
+    while(number > 0){
+        zifra = number % 10;
+
+        if (zifra == 0){
+            nullCount = nullCount + 1;
+        }
+
+        number /= 10;
+    }
+
+    return nullCount;
+
+}
+
+bool itc_mirror_num(long long number){/////////////////////
+bool zifra,x,y;
+    while (number > 0){
+        zifra = number % 10;
+        zifra =
+
+
+    }
+
+
+
+
